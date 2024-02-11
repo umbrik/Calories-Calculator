dd59f200b9b14d3d8309c2e8ba767c7bbc7d3217
Ivan Duplenskikh <idumbrik@gmail.com>
2024-02-11 22:33:21 +0100

    Add Models for income parameters

--- Backend/Controllers/FoodController.cs
+++ Backend/Controllers/FoodController.cs
@@ -1,5 +1,6 @@
 ﻿using Calories_Calculator.Database;
 using Calories_Calculator.Entities;
+using Calories_Calculator.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Calories_Calculator.Controllers;
@@ -19,87 +20,52 @@ public class FoodController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Food> Get([FromBody] Food foodParams)
+    public IEnumerable<Food> Get([FromBody] GetFood? food)
     {
-        Console.WriteLine(foodParams.Id);
-        return _context.Foods.ToList();
+        if (food?.Id is null)
+        {
+            return _context.Foods.ToList();
+        }
+        else
+        {
+            return _context.Foods.Where(x => x.Id == food.Id).ToList();
+        }
     }
 
     [HttpPut]
-    public bool Put([FromQuery] string name,
-        [FromQuery] float calories,
-        [FromQuery] float protein,
-        [FromQuery] float fat,
-        [FromQuery] float carbohydrate,
-        [FromQuery] float fiber,
-        [FromQuery] float sugar)
+    public bool Put([FromBody] PutFood food)
     {
         _context.Add<Food>(new()
         {
-            Name = name,
-            Calories = calories,
-            Protein = protein,
-            Fat = fat,
-            Carbohydrate = carbohydrate,
-            Fiber = fiber,
-            Sugar = sugar,
+            Name = food.Name,
+            Calories = food.Calories,
+            Protein = food.Protein,
+            Fat = food.Fat,
+            Carbohydrate = food.Carbohydrate,
+            Fiber = food.Fiber,
+            Sugar = food.Sugar,
         });
 
         return _context.SaveChanges() == 1;
     }
 
     [HttpPatch]
-    public IActionResult Patch([FromQuery] int id,
-        [FromQuery] string? name,
-        [FromQuery] float? calories,
-        [FromQuery] float? protein,
-        [FromQuery] float? fat,
-        [FromQuery] float? carbohydrate,
-        [FromQuery] float? fiber,
-        [FromQuery] float? sugar)
+    public IActionResult Patch([FromBody] PatchFood food)
     {
-        var updatedFood = _context.Foods.FirstOrDefault(x => x.Id == id);
+        var foodEntity = _context.Foods.Find(food.Id);
 
-        if (updatedFood is null)
+        if (foodEntity is null)
         {
             return NotFound();
         }
 
-        if (name is not null)
-        {
-            updatedFood.Name = name;
-        }
-
-        if (calories is not null)
-        {
-            updatedFood.Calories = (float)calories;
-        }
-
-        if (protein is not null)
-        {
-            updatedFood.Protein = (float)protein;
-        }
-
-        if (fat is not null)
-        {
-            updatedFood.Fat = (float)fat;
-        }
-
-        if (carbohydrate is not null)
-        {
-            updatedFood.Carbohydrate = (float)carbohydrate;
-        }
-
-        if (fiber is not null)
-        {
-            updatedFood.Fiber = (float)fiber;
-        }
-
-        if (sugar is not null)
-        {
-            updatedFood.Sugar = (float)sugar;
-        }
-
+        foodEntity.Name = food.Name ?? foodEntity.Name;
+        foodEntity.Calories = food.Calories ?? foodEntity.Calories;
+        foodEntity.Protein = food.Protein ?? foodEntity.Protein;
+        foodEntity.Fat = food.Fat ?? foodEntity.Fat;
+        foodEntity.Carbohydrate = food.Carbohydrate ?? foodEntity.Carbohydrate;
+        foodEntity.Fiber = food.Fiber ?? foodEntity.Fiber;
+        foodEntity.Sugar = food.Sugar ?? foodEntity.Sugar;
         return Ok(_context.SaveChanges() == 1);
     }
 }
--- Backend/Models/Food.cs
+++ Backend/Models/Food.cs
@@ -1,13 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Calories_Calculator.Models;
 
-public class Food : BaseEntity
+public class GetFood
 {
-    public int Id { get; set; }
-    public required string Name { get; set; }
-    public required float Calories { get; set; }
-    public required float Protein { get; set; }
-    public required float Fat { get; set; }
-    public required float Carbohydrate { get; set; }
-    public required float Fiber { get; set; }
-    public required float Sugar { get; set; }
+    public int? Id { get; set; }
+}
+
+public class PutFood
+{
+    [MinLength(3)]
+    public string Name { get;set; }
+    public float Calories { get;set; }
+    public float Protein { get;set; }
+    public float Fat { get;set; }
+    public float Carbohydrate { get;set; }
+    public float Fiber { get;set; }
+    public float Sugar { get;set; }
+}
+
+public class PatchFood
+{
+    public int Id { get;set; }
+    public string? Name { get;set; }
+    public float? Calories { get;set; }
+    public float? Protein { get;set; }
+    public float? Fat { get;set; }
+    public float? Carbohydrate { get;set; }
+    public float? Fiber { get;set; }
+    public float? Sugar { get;set; }
 }

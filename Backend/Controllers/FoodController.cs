5579993a9a9441af1e1377f756c8e3439c9d0078
Ivan Duplenskikh <idumbrik@gmail.com>
2024-02-11 20:59:56 +0100

    Add Put and Patch methods to Food controller

--- Backend/Controllers/FoodController.cs
+++ Backend/Controllers/FoodController.cs
@@ -23,4 +23,82 @@ public class FoodController : ControllerBase
     {
         return _context.Foods.ToList();
     }
+
+    [HttpPut]
+    public bool Put([FromQuery] string name,
+        [FromQuery] float calories,
+        [FromQuery] float protein,
+        [FromQuery] float fat,
+        [FromQuery] float carbohydrate,
+        [FromQuery] float fiber,
+        [FromQuery] float sugar)
+    {
+        _context.Add<Food>(new()
+        {
+            Name = name,
+            Calories = calories,
+            Protein = protein,
+            Fat = fat,
+            Carbohydrate = carbohydrate,
+            Fiber = fiber,
+            Sugar = sugar,
+        });
+
+        return _context.SaveChanges() == 1;
+    }
+
+    [HttpPatch]
+    public IActionResult Patch([FromQuery] int id,
+        [FromQuery] string? name,
+        [FromQuery] float? calories,
+        [FromQuery] float? protein,
+        [FromQuery] float? fat,
+        [FromQuery] float? carbohydrate,
+        [FromQuery] float? fiber,
+        [FromQuery] float? sugar)
+    {
+        var updatedFood = _context.Foods.FirstOrDefault(x => x.Id == id);
+
+        if (updatedFood is null)
+        {
+            return NotFound();
+        }
+
+        if (name is not null)
+        {
+            updatedFood.Name = name;
+        }
+
+        if (calories is not null)
+        {
+            updatedFood.Calories = (float)calories;
+        }
+
+        if (protein is not null)
+        {
+            updatedFood.Protein = (float)protein;
+        }
+
+        if (fat is not null)
+        {
+            updatedFood.Fat = (float)fat;
+        }
+
+        if (carbohydrate is not null)
+        {
+            updatedFood.Carbohydrate = (float)carbohydrate;
+        }
+
+        if (fiber is not null)
+        {
+            updatedFood.Fiber = (float)fiber;
+        }
+
+        if (sugar is not null)
+        {
+            updatedFood.Sugar = (float)sugar;
+        }
+
+        return Ok(_context.SaveChanges() == 1);
+    }
 }
